cc79a75c83aceaf505a8b484a076b45d620fc5d9
Rodrigo de Farias Ramires <rodrigoramiresbr94@gmail.com>
2021-06-27 13:12:11 -0300

    MODULE #12 Code Quality with ESLint

--- 12 - Code Quality with ESLint/bad-code.js
+++ 12 - Code Quality with ESLint/bad-code.js
@@ -1,10 +1,12 @@
-var weather = new Promise((resolve) => {
+/* globals ga twttr */
+
+const weather = new Promise((resolve) => {
   setTimeout(() => {
     resolve({ temp: 29, conditions: 'Sunny with Clouds' });
   }, 2000);
 });
 
-const tweets = new Promise((resolve, reject) => {
+const tweets = new Promise((resolve) => {
   setTimeout(() => {
     resolve(['I like cake', 'BBQ is good too!']);
   }, 500);
@@ -12,7 +14,7 @@ const tweets = new Promise((resolve, reject) => {
 
 Promise
   .all([weather, tweets])
-  .then(responses => {
+  .then((responses) => {
     const [weatherInfo, tweetInfo] = responses;
     console.log(weatherInfo, tweetInfo);
   });
@@ -22,9 +24,59 @@ const streetCarsPromise = fetch('http://data.ratp.fr/api/datasets/1.0/search/?q=
 
 Promise
   .all([postsPromise, streetCarsPromise])
-  .then(responses => {
-    return Promise.all(responses.map(res => res.json()));
-  })
-  .then(responses => {
+  .then((responses) => Promise.all(responses.map((res) => res.json())))
+  .then((responses) => {
     console.log(responses);
   });
+
+ga.track();
+twttr.trackConversion();
+
+/* eslint-disable */
+if (!Array.prototype.includes) {
+  Object.defineProperty(Array.prototype, 'includes', {
+    value(searchElement, fromIndex) {
+      // 1. Let O be ? ToObject(this value).
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      const o = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(O, "length")).
+      const len = o.length >>> 0;
+
+      // 3. If len is 0, return false.
+      if (len === 0) {
+        return false;
+      }
+
+      // 4. Let n be ? ToInteger(fromIndex).
+      //    (If fromIndex is undefined, this step produces the value 0.)
+      const n = fromIndex | 0;
+
+      // 5. If n ≥ 0, then
+      //  a. Let k be n.
+      // 6. Else n < 0,
+      //  a. Let k be len + n.
+      //  b. If k < 0, let k be 0.
+      let k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+      // 7. Repeat, while k < len
+      while (k < len) {
+        // a. Let elementK be the result of ? Get(O, ! ToString(k)).
+        // b. If SameValueZero(searchElement, elementK) is true, return true.
+        // c. Increase k by 1.
+        // NOTE: === provides the correct "SameValueZero" comparison needed here.
+        if (o[k] === searchElement) {
+          return true;
+        }
+        k++;
+      }
+
+      // 8. Return false
+      return false;
+    },
+  });
+}
+/* eslint-enable */
